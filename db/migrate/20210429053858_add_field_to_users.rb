5b5216eee7a7896c3b15946eb07b784477539c15
Akintola Ramota <akintolarahmah@gmail.com>
2021-04-29 05:56:57 +0000

    Add gravatar

--- db/migrate/20210429053858_add_field_to_users.rb
+++ db/migrate/20210429053858_add_field_to_users.rb
@@ -1,4 +1,4 @@
-class AddNameToUsers < ActiveRecord::Migration[6.1]
+class AddFieldToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :name, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_044005) do
+ActiveRecord::Schema.define(version: 2021_04_29_053858) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
